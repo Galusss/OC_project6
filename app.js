033309b505b893f25e4fe6df648b83f73db63139
Galusss <gael.paquien.contact@gmail.com>
2020-07-10 17:53:04 +0200

    add : MongoDB Atlas / f  Models / f Routes / f Controllers

--- app.js
+++ app.js
@@ -1,6 +1,38 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+// const Sauce = require('../models/sauce.js'); // Importe le schéma des données de sauce
+// const User = require ('./models/user.js'); // Importe le schéma des données de user
+
+const sauceRoutes = require('./routes/sauces'); // Importe le routeur sauceRoutes
+
+mongoose.connect('mongodb+srv://superadmin:456.password@apibdd.nkz4u.mongodb.net/<dbname>?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = express();
+
+app.use((req, res, next) => { // Création d'un middleware qui permet d'acceder a l'API depuis n'importe quelle origine, ajouter les header, envoyer des requête avec les methode mentionnées ; cela évitera les erreurs CORS "Cross Origin Ressource Sharing"
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use(bodyParser.json());
+
+app.use('/api/sauces', sauceRoutes) // Toute les demandes effectuer vers /api/sauces seront rediriger vers le routeur sauceRoutes
+
+
+
+
+
+
 
 app.use((req, res, next) => { 
     console.log('Requête reçue !');
@@ -13,12 +45,14 @@ app.use((req, res, next) => {
 });
 
 app.use((req, res, next) => {
-    res.json({ message: 'Votre requête a bien été reçue !' });
+    res.json({ message: 'Serveur OK !' });
     next();
 });
 
 app.use((req, res, next) => {
     console.log('Réponse envoyée avec succès !');
 });
+
+
 
 module.exports = app;
---
+++ controllers/sauce.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const stuffCtrl = require('../controllers/stuff');
+
+// LOGIQUE METIERS DE NOS ROUTES
+
+module.exports = router;
